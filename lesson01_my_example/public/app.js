0fc02a2aa20a4f0d27af7cdd896b28058bd846ff
RSatanovskyi <satcms@hotmail.com>
2026-06-26 13:02:34 +0300

    create, load, edit and delete comments.

--- lesson01_my_example/public/app.js
+++ lesson01_my_example/public/app.js
@@ -1,4 +1,5 @@
 const API_URL = "/movies";
+const API_COMMENTS = "/comments";
 
 const movieList = document.getElementById("movieList");
 
@@ -26,4 +27,97 @@ async function loadMovies() {
   });
 }
 
+async function loadComments() {
+  const response = await fetch(API_COMMENTS);
+
+  const comments = await response.json();
+
+  const commentsList = document.getElementById("commentsList");
+
+  commentsList.innerHTML = "";
+
+  comments.forEach((comment) => {
+    const article = document.createElement("article");
+
+    article.classList.add("comment-card");
+
+    article.innerHTML = `
+  <div class="comment-header">
+    <h3>${comment.user}</h3>
+  </div>
+    <p>${comment.text}</p>
+    <div class="comment-actions">
+    <button
+      class="edit-btn"
+      data-id="${comment.id}"
+    >
+      Edit
+    </button>
+
+    <button
+      class="delete-btn"
+      data-id="${comment.id}"
+    >
+      Delete
+    </button>
+    </div>
+    `;
+
+    commentsList.appendChild(article);
+  });
+}
+
 loadMovies();
+loadComments();
+
+document.addEventListener("click", async (e) => {
+  if (e.target.matches(".delete-btn")) {
+    const id = e.target.dataset.id;
+
+    await deleteComment(id);
+  }
+
+  if (e.target.matches(".edit-btn")) {
+    const id = e.target.dataset.id;
+
+    await editComment(id);
+  }
+});
+
+async function deleteComment(id) {
+  const response = await fetch("http://localhost:3000/comments/" + id, {
+    method: "DELETE",
+  });
+
+  if (!response.ok) {
+    throw new Error("Delete failed");
+  }
+
+  console.log("Deleted comment:", id);
+
+  loadComments();
+}
+
+async function editComment(id) {
+  const newText = prompt("Edit your comment:");
+
+  if (!newText) return;
+
+  const response = await fetch("http://localhost:3000/comments/" + id, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      text: newText,
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error("Edit failed");
+  }
+
+  console.log("Updated comment:", id);
+
+  loadComments();
+}
--- lesson01_my_example/public/movie.js
+++ lesson01_my_example/public/movie.js
@@ -4,6 +4,8 @@ const params = new URLSearchParams(window.location.search);
 
 const id = params.get("id");
 
+const commentForm = document.getElementById("comment-form");
+
 async function loadMovie() {
   const response = await fetch(API_URL + "/" + id);
 
@@ -48,3 +50,24 @@ async function loadMovie() {
 }
 
 loadMovie();
+
+commentForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const user = document.getElementById("comment-user").value;
+
+  const text = document.getElementById("comment-input").value;
+
+  const response = await fetch("http://localhost:3000/comments", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id,
+      user,
+      text,
+    }),
+  });
+});
+
