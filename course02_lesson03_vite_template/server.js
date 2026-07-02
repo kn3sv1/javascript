6a4789f999974ddd02c18e24d9d87a50d774808c
RSatanovskyi <satcms@hotmail.com>
2026-07-02 21:47:03 +0300

    dist removed and replaced with public

--- course02_lesson03_vite_template/server.js
+++ course02_lesson03_vite_template/server.js
@@ -1,33 +1,33 @@
-const express = require('express')
-const jsonServer = require('json-server')
-const path = require('path')
+const express = require("express");
+const jsonServer = require("json-server");
+const path = require("path");
 const files = require("./files");
 const fs = require("fs");
 
-const app = express()
+const app = express();
 
-const router = jsonServer.router('db.json')
+const router = jsonServer.router("db.json");
 
 // API
-app.use('/api', router);
+app.use("/api", router);
 //FILES to upload and get list from folder
 app.use("/files", files);
 
 // static
-app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, "public")));
 
 // SPA fallback admin area
-app.get('/admin/*', (req, res) => {
+app.get("/admin/*", (req, res) => {
   // console.log(req.path);
-  res.sendFile(path.join(__dirname, 'dist', 'admin/index.html'))
-})
+  res.sendFile(path.join(__dirname, "public", "admin/index.html"));
+});
 
 // SPA fallback
-app.get('*', (req, res) => {
+app.get("*", (req, res) => {
   // console.log(req.path);
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
-})
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.listen(3000, () => {
-  console.log('http://localhost:3000')
-})
+  console.log("http://localhost:3000");
+});
---
+++ course02_lesson04_calculator/README.txt
@@ -0,0 +1,55 @@
+3)
+npm run dev 
+
+http://localhost:5173/about.html
+http://localhost:5173/
+
+
+
+
+2)
+http://localhost:5173/about.html
+http://localhost:5173/
+http://localhost:5173/index.html
+
+
+http://localhost:3000/api/cats
+
+2.1) DEV
+run project:
+npm run dev
+
+2.2) TEST
+npm run test
+ 
+2.3) BROWSER compiled after Vite JS files
+npm run build
+npm run server
+
+http://localhost:5173/about.html
+http://localhost:5173/
+http://localhost:5173/index.html
+
+
+1)
+This will launch our static file index.html
+http://localhost:3000/
+
+
+this is API post means we have in JSON file post BLOCK
+http://localhost:3000/api/posts
+
+
+json-server --watch db.json --routes routes.json
+
+
+# Only works with this versions
+npm install express@4
+npm install json-server@0.17.4
+
+npm install multer
+
+
+Photos for POSTMAN
+E:\projects\java\Course09\lesson13_medical_clinic01\public\images\cats
+
