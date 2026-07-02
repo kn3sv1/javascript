ec10b6665f1ac6a82da21f331b69e1f0988bc81d
RSatanovskyi <satcms@hotmail.com>
2026-07-02 17:48:46 +0300

    vscode DEBUG open FILE

--- course02_lesson03_typescript_simple/tests/example1.test.ts
+++ course02_lesson03_typescript_simple/tests/example1.test.ts
@@ -2,6 +2,9 @@ import { describe, it, expect } from "vitest";
 
 describe("math", () => {
   it("adds numbers", () => {
+    let a = 123;
+    let b = 234;
+    let c = a + b;
     expect(1 + 1).toBe(2);
   });
 });
