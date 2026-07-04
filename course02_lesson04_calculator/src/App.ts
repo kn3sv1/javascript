d3b96c11152aa66b2f9dfa64a14494d7eaacf7f3
RSatanovskyi <satcms@hotmail.com>
2026-07-04 19:31:50 +0300

    lesson 4.1 typescript

--- course02_lesson04_calculator/src/App.ts
+++ course02_lesson04_calculator/src/App.ts
@@ -1,2 +1,19 @@
-
 console.log("Hello App");
+
+// https://nodejs.org/learn/typescript/introduction
+type User = {
+  name: string;
+  age: number;
+};
+
+function isAdult(user: User): boolean {
+  return user.age >= 18;
+}
+
+const angie: User = {
+  name: "Angie",
+  age: 29,
+};
+const angieAdult = isAdult(angie);
+
+console.log(`angieAdult: ${angieAdult}`);
