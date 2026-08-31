bce27c3f990d4d1364871f76b4a9239c4f981f4d
RSatanovskyi <satcms@hotmail.com>
2026-08-31 20:51:15 +0300

    lesson 15. Multer + static file server

--- course02_lesson15_doctors_of_cyprus_step_by_step/index.js
+++ course02_lesson15_doctors_of_cyprus_step_by_step/index.js
@@ -1,13 +1,84 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
-const { homePage, angiePage, showFormPage, commentsPage } = require("./lib/pages");
+const {
+  homePage,
+  angiePage,
+  showFormPage,
+  commentsPage,
+  showUploadFilePage,
+  showErrorPage,
+} = require("./lib/pages");
 const { getFormData } = require("./lib/formData");
 const { saveData, readData } = require("./lib/database");
+const { uploadDoctorPhoto, UPLOADS_DIR } = require("./lib/upload");
 
 const PORT = 3000;
+const PUBLIC_DIR = path.join(__dirname, "public");
+
+const MIME_TYPES = {
+  ".css": "text/css",
+  ".js": "text/javascript",
+  ".png": "image/png",
+  ".jpg": "image/jpeg",
+  ".jpeg": "image/jpeg",
+  ".gif": "image/gif",
+  ".webp": "image/webp",
+  ".svg": "image/svg+xml",
+};
+
+function sendHtml(res, status, html) {
+  res.writeHead(status, { "Content-Type": "text/html; charset=utf-8" });
+  res.end(html);
+}
+
+function redirect(res, location) {
+  res.writeHead(302, { Location: location });
+  res.end();
+}
+
+function serveStatic(req, res, rootDir, urlPrefix) {
+  const pathname = new URL(req.url, "http://localhost").pathname;
+  const relPath = decodeURIComponent(pathname.slice(urlPrefix.length));
+  const filePath = path.join(rootDir, relPath);
+
+  if (!filePath.startsWith(rootDir)) {
+    sendHtml(res, 403, "Forbidden");
+    return;
+  }
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      sendHtml(res, 404, "Not found");
+      return;
+    }
+    const ext = path.extname(filePath).toLowerCase();
+    res.writeHead(200, {
+      "Content-Type": MIME_TYPES[ext] || "application/octet-stream",
+    });
+    res.end(data);
+  });
+}
 
 const server = http.createServer(async (req, res) => {
+  let parsed;
+  try {
+    parsed = new URL(req.url, "http://localhost");
+  } catch (err) {
+    return sendHtml(res, 400, "Bad request");
+  }
+
+  // http://localhost:3000/uploads/doctors/keyboard.png
+  // http://localhost:3000/public/style.css
+  // http://localhost:3000/public/hello.js
+  const pathname = parsed.pathname || "/";
+  if (pathname.startsWith("/public/")) {
+    return serveStatic(req, res, PUBLIC_DIR, "/public/");
+  }
+  if (pathname.startsWith("/uploads/")) {
+    return serveStatic(req, res, UPLOADS_DIR, "/uploads/");
+  }
+
   if (req.method === "POST" && req.url === "/submit") {
     const formData = await getFormData(req, res);
     console.log(formData);
@@ -22,6 +93,27 @@ const server = http.createServer(async (req, res) => {
 
   if (req.url === "/form") {
     showFormPage(res);
+    return;
+  }
+
+  if (req.url === "/show-upload") {
+    showUploadFilePage(res);
+    return;
+  }
+
+  if (req.url === "/upload") {
+    uploadDoctorPhoto(req, res, (err) => {
+      if (err) {
+        return showErrorPage(res, err);
+      }
+
+      console.log(req.file.filename);
+      console.log(req.file);
+
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end("Received uploaded file!");
+    });
+
     return;
   }
 
--- course02_lesson15_doctors_of_cyprus_step_by_step/lib/pages.js
+++ course02_lesson15_doctors_of_cyprus_step_by_step/lib/pages.js
@@ -12,9 +12,20 @@ function showFormPage(res) {
   res.writeHead(200, { "Content-Type": "text/html" });
   res.end(`
       <form method="POST" action="/submit">
-        <input name="name" />
-        <input name="message" />
+        <input name="name" /><br>
+        <input name="message" /><br>
         <button type="submit">Send</button>
+      </form>
+      `);
+}
+
+function showUploadFilePage(res) {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end(`
+      <form action="/upload" method="POST" enctype="multipart/form-data">
+        <label for="file">Choose a file:</label><br><br>
+        <input type="file" id="file" name="file"><br><br>
+        <button type="submit">Upload</button>
       </form>
       `);
 }
@@ -30,9 +41,16 @@ function commentsPage(res, comments) {
   res.end(html);
 }
 
+function showErrorPage(res, err) {
+  res.writeHead(400, { "Content-Type": "text/html" });
+  res.end(`ErrorPage ${err}`);
+}
+
 module.exports = {
   homePage,
   angiePage,
   showFormPage,
   commentsPage,
+  showUploadFilePage,
+  showErrorPage,
 };
